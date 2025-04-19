47365b1a0830425b16a38e4f383f298e6cd81bbc
Shah Nawaz <shuvo1824@hotmail.com>
2025-04-19 14:21:16 +0600

    fix: docker setups removed

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -13,14 +13,11 @@ export default defineConfig({
     },
     preserveSymlinks: true
   },
-  optimizeDeps: {
-    include: ["hono/client"]
-  },
   server: {
     port: 3000,
     proxy: {
       "/api": {
-        target: "http://server:8080",
+        target: "http://localhost:8080",
         changeOrigin: true,
       },
     },
