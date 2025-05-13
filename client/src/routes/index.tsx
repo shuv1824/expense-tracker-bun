caf96f7ae57625064b797786311eb6fa376fbe26
Shah Nawaz <shuvo1824@hotmail.com>
2025-05-13 12:41:59 +0600

    fix: tanstack router issue

--- client/src/routes/index.tsx
+++ client/src/routes/index.tsx
@@ -1,13 +1,46 @@
 import { createFileRoute } from '@tanstack/react-router'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { useQuery } from "@tanstack/react-query";
+import { api } from "@/lib/api";
 
-export const Route = createFileRoute()({
+export const Route = createFileRoute('/')({
   component: Index,
 })
 
-function Index() {
-  return (
-    <div className="p-2">
-      <h3>Welcome Home!</h3>
-    </div>
-  )
+async function getTotalSpent() {
+  const res = await api.expenses["total-spent"].$get();
+  if (!res.ok) {
+    throw new Error("Server error.");
+  }
+  const data = await res.json();
+  return data;
 }
+
+function Index() {
+  const { isPending, error, data } = useQuery({
+    queryKey: ['get-total-spent'],
+    queryFn: getTotalSpent
+  })
+
+  if (isPending) return "Loading..."
+
+  if (error) return "An error has occured: " + error.message
+  return (
+    <>
+      <Card className="w-[350px] m-auto">
+        <CardHeader>
+          <CardTitle>Total Spent</CardTitle>
+          <CardDescription>The total amount you have spent.</CardDescription>
+        </CardHeader>
+        <CardContent>{data.total}</CardContent>
+      </Card>
+    </>
+  );
+}
+
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,10 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
-import { tanstackRouter } from "@tanstack/router-vite-plugin";
+import { tanstackRouter } from "@tanstack/router-plugin/vite";
 
 export default defineConfig({
-  plugins: [react(), tanstackRouter()],
+  plugins: [
+    tanstackRouter({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
