046e3c253b235027e4f9e790b0370646be67b627
Shah Nawaz <shuvo1824@hotmail.com>
2025-10-21 23:30:04 +0600

    fix: minor css issue on create expense page

--- client/src/routes/create-expense.tsx
+++ client/src/routes/create-expense.tsx
@@ -1,6 +1,7 @@
 import { createFileRoute } from '@tanstack/react-router'
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
+import { Button } from "@/components/ui/button"
 
 export const Route = createFileRoute('/create-expense')({
   component: CreateExpense,
@@ -8,13 +9,13 @@ export const Route = createFileRoute('/create-expense')({
 
 function CreateExpense() {
   return <div className="p-2">
-    <h2>Create new expense</h2> 
-    <form>
+    <h2>Create Expense</h2> 
+    <form className="max-w-xl m-auto">
       <Label htmlFor="title">Title</Label>
       <Input id="title" type="text" placeholder="Title" />
       <Label htmlFor="amount">Amount</Label>
       <Input id="amount" type="number" placeholder="Amount" />
-      <button type="submit">Create Expense</button>
+      <Button className='mt-4' type="submit">Create Expense</Button>
     </form>
   </div>
 }
