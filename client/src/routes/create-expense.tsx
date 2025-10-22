17c89eb076e1680794497fc1c679e26b8a6da46a
Shah Nawaz <shuvo1824@hotmail.com>
2025-10-22 22:25:01 +0600

    chore: tanstack form added

--- client/src/routes/create-expense.tsx
+++ client/src/routes/create-expense.tsx
@@ -2,20 +2,101 @@ import { createFileRoute } from '@tanstack/react-router'
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 import { Button } from "@/components/ui/button"
+import { useForm } from '@tanstack/react-form'
+import type { AnyFieldApi } from '@tanstack/react-form'
 
 export const Route = createFileRoute('/create-expense')({
   component: CreateExpense,
 })
 
+
+function FieldInfo({ field }: { field: AnyFieldApi }) {
+  return (
+    <>
+      {field.state.meta.isTouched && !field.state.meta.isValid ? (
+        <em>{field.state.meta.errors.join(', ')}</em>
+      ) : null}
+      {field.state.meta.isValidating ? 'Validating...' : null}
+    </>
+  )
+}
+
 function CreateExpense() {
+  const form = useForm({
+    defaultValues: {
+      title: '',
+      amount: 0,
+    },
+    onSubmit: async ({ value }) => {
+      // Do something with form data
+      console.log(value)
+    },
+  })
+
   return <div className="p-2">
     <h2>Create Expense</h2> 
-    <form className="max-w-xl m-auto">
-      <Label htmlFor="title">Title</Label>
-      <Input id="title" type="text" placeholder="Title" />
-      <Label htmlFor="amount">Amount</Label>
-      <Input id="amount" type="number" placeholder="Amount" />
-      <Button className='mt-4' type="submit">Create Expense</Button>
+    <form
+        onSubmit={(e) => {
+          e.preventDefault()
+          e.stopPropagation()
+          form.handleSubmit()
+        }} className="max-w-xl m-auto"
+      >
+        <form.Field
+            name="title"
+            validators={{
+              onChange: ({ value }) =>
+                !value
+                  ? 'A title is required'
+                  : value.length < 3
+                    ? 'Title must be at least 3 characters'
+                    : undefined,
+              onChangeAsyncDebounceMs: 500,
+            }}
+            children={(field) => {
+              // Avoid hasty abstractions. Render props are great!
+              return (
+                <>
+                  <Label htmlFor={field.name}>Title</Label>
+                  <Input
+                    id={field.name}
+                    name={field.name}
+                    value={field.state.value}
+                    onBlur={field.handleBlur}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                  />
+                  <FieldInfo field={field} />
+                </>
+              )
+            }}
+          />
+        <form.Field
+            name="amount"
+            children={(field) => {
+              // Avoid hasty abstractions. Render props are great!
+              return (
+                <>
+                  <Label htmlFor={field.name}>Amount</Label>
+                  <Input
+                    id={field.name}
+                    name={field.name}
+                    value={field.state.value}
+                    onBlur={field.handleBlur}
+                    onChange={(e) => field.handleChange(Number(e.target.value))}
+                  />
+                  <FieldInfo field={field} />
+                </>
+              )
+            }}
+          />
+        <form.Subscribe
+          selector={(state) => [state.canSubmit, state.isSubmitting]}
+          children={([canSubmit, isSubmitting]) => (
+            <Button className='mt-4' type="submit" disabled={!canSubmit}>
+              {isSubmitting ? '...' : 'Submit'}
+            </Button>
+          )}
+        />
     </form>
   </div>
 }
